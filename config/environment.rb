b188e1fa9ea11b666dd5980c6eafdb8d7980c195
StevenNdaye <stevenndaye@gmail.com>
2014-01-29 19:16:11 -0200

    Modified the environment file

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,18 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Depot::Application.initialize!
 
+Depot::Application.configure do
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: ENV["G_USERNAME"],
+    password: ENV["G_PASSWORD"],
+    enable_starttls_auto: true
+  }
+
+end
+
